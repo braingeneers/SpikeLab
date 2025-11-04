42821a25880b13d36fd7e538a61843b19b5439e3
Luka <lukacheney@berkeley.edu>
2025-11-03 18:05:44 -0800

    simple version of ratedata

---
+++ spikedata/ratedata.py
@@ -0,0 +1,40 @@
+class RateData:
+    def __init__(
+        self,
+        inst_Frate_data,
+        subset_neurons = [],
+        subset_time_range = [],
+        N = None,
+        length = None,
+    ):
+        self.inst_Frate_data = inst_Frate_data
+        if self.inst_Frate_data.ndim != 2:
+            raise ValueError(f"rates must be a 2D array, got shape {self.inst_Frate_data.shape}")
+        
+        self.N = inst_Frate_data.shape[0]
+        self.length = inst_Frate_data.shape[1]
+        self.subset_neurons = range(self.N)
+        self.subset_time_range = (0,length-1)
+
+
+    def subset(self, units):
+        
+        output = self.inst_Frate_data[units,:]
+        # for neuron in units:
+        #     neuron_firing_rate = self.inst_Frate_data[neuron,:]
+        #     output.append(neuron_firing_rate)
+        #     neuron_ids.append(neuron)
+        return RateData(
+            inst_Frate_data = output,
+            subset_neurons = units,
+            subset_time_range = self.subset_time_range
+        )
+    def subtime(self, start, end):
+        output = self.inst_Frate_data[:,start:end]
+        return RateData(
+            inst_Frate_data = output,
+            subset_neurons = self.subset_neurons,
+            subset_time_range = (start, end)
+        )
+
+
